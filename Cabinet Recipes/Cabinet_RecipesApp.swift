dc94bb77be8968d29e2b7262287f925b07640a73
Jordan Huffaker <jhuffaker@byui.edu>
2021-05-25 15:01:20 -0600

    Initial Commit

---
+++ Cabinet Recipes/Cabinet_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cabinet_RecipesApp.swift
+//  Cabinet Recipes
+//
+//  Created by Jordan Huffaker on 5/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Cabinet_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
