642f7f2de34e5ef051b6a6d60f1ca5be76bece42
Jordan Huffaker <jhuffaker@byui.edu>
2021-06-14 07:42:15 -0600

    Add SettingsView.

---
+++ Cabinet Recipes/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Cabinet Recipes
+//
+//  Created by Jordan Huffaker on 6/14/21.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
