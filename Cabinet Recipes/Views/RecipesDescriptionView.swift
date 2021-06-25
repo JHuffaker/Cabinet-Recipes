774ceefca678f2098ea6166d69fd121b0cc0fdb9
Jordan Huffaker <jhuffaker@byui.edu>
2021-06-25 07:44:47 -0600

    Clean up the Description View. Add comments. Remove delete button.

--- Cabinet Recipes/Views/RecipesDescriptionView.swift
+++ Cabinet Recipes/Views/RecipesDescriptionView.swift
@@ -10,78 +10,30 @@ import Firebase
 
 struct RecipesDescriptionView: View {
     var recipe: Recipe
-    @State private var deleteAlert = false
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
 
-            ScrollView {
-                ZStack {
-                    VStack(alignment: .leading) {
-                        
-                        Text("Ingredients")
-                            .font(.title2)
-                            .padding(.top)
-                        ForEach(recipe.ingredients, id: \.self) { ingredient in
-                            HStack(alignment: .top) {
-                                Text("• ")
-                                Text(ingredient)
-                            }
-                        }
-                        
-                        Divider()
-                        
-                        Text("Directions")
-                            .font(.title2)
-                        ForEach(0..<recipe.directions.count) { index in
-                            HStack(alignment: .top) {
-                                Text("\(index + 1).")
-                                Text(recipe.directions[index])
-                            }
-                        }
-                        Spacer()
-                    }.padding([.leading, .trailing], 20)
-                    .navigationBarTitle(recipe.name)
-                    .navigationBarItems(trailing: NavigationLink(destination: EditRecipeView(recipe: recipe)) {
-                        Text("Edit")
-                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                        }
-                    )
-                    .background(
-                        Rectangle()
-                            .foregroundColor(.white)
-                            .padding([.leading, .trailing], 10)
-                            .opacity(/*@START_MENU_TOKEN@*/0.9/*@END_MENU_TOKEN@*/)
-                    )
-                    .toolbar {
-                        ToolbarItemGroup(placement: .bottomBar) {
-                            Button("Delete") {
-                                        self.deleteAlert = true
-                            }
-                            Spacer()
-                        }
-                    }.alert(isPresented: $deleteAlert) {
-                        Alert(title: Text("Are you sure you want to delete this?"), message: Text("You will not be able to undo this action"), primaryButton: .destructive(Text("Delete")) {
-                            deleteRecipe(docName: recipe.docName)
-                        },
-                        secondaryButton: .cancel())
-                    }
+        Form {
+            // Display the ingredients
+            Section(header: Text("Ingredients")) {
+                ForEach(recipe.ingredients, id: \.self) {
+                    Text($0)
                 }
             }
-//            .background(CircleImage())
-    }
-    
-    func deleteRecipe(docName: String) {
-        let db = Firestore.firestore()
-        let documentName = db.collection("sub recipes").document(docName)
-        documentName.delete() { error in
-            if let error = error {
-                print(error.localizedDescription)
-            } else {
-                return
+            // Display the instructions
+            Section(header: Text("Instructions")) {
+                ForEach(0..<recipe.directions.count) {
+                    Text("\($0 + 1). \(recipe.directions[$0])")
+                }
             }
-        }
-        presentationMode.wrappedValue.dismiss()
+        }.navigationBarTitle(recipe.name)
+        // Edit button
+        .navigationBarItems(trailing: NavigationLink(destination: EditRecipeView(recipe: recipe)) {
+                Text("Edit")
+                .foregroundColor(.blue)
+            }
+        )
     }
 }
 
