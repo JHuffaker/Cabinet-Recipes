49df1ed69fc361b71c1e73c859eb351d077f8f3c
Jordan Huffaker <jhuffaker@byui.edu>
2021-06-25 14:15:07 -0600

    Edits the ingredients and instructions as well.

--- Cabinet Recipes/Views/EditRecipeView.swift
+++ Cabinet Recipes/Views/EditRecipeView.swift
@@ -9,63 +9,106 @@ import SwiftUI
 import FirebaseFirestore
 
 struct EditRecipeView: View {
-    var recipe: Recipe
-    @State var newName = ""
-    @State var newIngredients = [String]()
-    @State var newDirections = [String]()
+    @State var recipe: Recipe
+    @State var name = ""
+    @State var deleteAlert = false
     @Environment(\.presentationMode) var presentationMode
     
-    init(recipe: Recipe) {
-        self.recipe = recipe
-        newName = self.recipe.name
-        newIngredients = self.recipe.ingredients
-        newDirections = self.recipe.directions
-    }
-    
     var body: some View {
-        NavigationView {
             Form {
-                Section {
-                    TextField(recipe.name, text: $newName)
+                // Allow user to edit name
+                Section(header: Text("Name")) {
+                    TextField(recipe.name, text: $name)
                 }
-//                Section {
-//                    ForEach(0..<recipe.ingredients.count) { index in
-//                        TextField(recipe.ingredients[index], text: $newIngredients[index])
-//                    }
-//                }
-//                Section {
-//                    ForEach(0..<recipe.directions.count) { index in
-//                        TextField(recipe.directions[index], text: $newDirections[index])
-//                    }
-//                }
+                // Allow user to edit the ingredients
+                Section(header: Text("Ingredients")) {
+                    ForEach(0..<recipe.ingredients.count) {
+                        TextField(recipe.ingredients[$0], text: $recipe.ingredients[$0])
+                    }
+                }
+                // Allow user to edit the instructions
+                Section(header: Text("Instructions")) {
+                    ForEach(0..<recipe.directions.count) {
+                        TextField("\($0 + 1). \(recipe.directions[$0])", text: $recipe.directions[$0])
+                    }
+                }
             }.navigationBarTitle("Edit Recipe")
-            .navigationBarItems(trailing: NavigationLink(destination: RecipesListView()) {
-                Text("Save")
-                    .padding(20)
-                    .foregroundColor(.blue)
-            }.simultaneousGesture(TapGesture().onEnded{
-                editRecipe(name: newName, directions: newDirections, ingredients: newIngredients, docName: recipe.docName)
-            })
-            )
-        }
+            // Edits the recipe
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button("Save") {
+                        editRecipe()
+                    }
+                }
+            }
+            // Deletes the recipe
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    Button("Delete") {
+                        self.deleteAlert = true
+                    }
+                }
+            }
+            // Alerts the user that the recipe is about to
+            // be deleted and confirms intention
+            .alert(isPresented: $deleteAlert) {
+                Alert(title: Text("Are you sure you want to delete this?"), message: Text("You will not be able to undo this action"), primaryButton: .destructive(Text("Delete")) {
+                    deleteRecipe(docName: recipe.docName)
+                }, secondaryButton: .cancel())
+            }
     }
     
-    func editRecipe(name: String, directions: [String], ingredients: [String], docName: String) {
+    // Deletes the recipe from the database
+    func deleteRecipe(docName: String) {
+        let db = Firestore.firestore()
+        let documentName = db.collection("sub recipes").document(docName)
+        documentName.delete() { error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                return
+            }
+        }
+        // Returns to the list view
+        presentationMode.wrappedValue.dismiss()
+    }
+    
+    // Edits the recipe in the database
+    func editRecipe() {
+        
+        var newName =  self.name.trimmingCharacters(in: .whitespacesAndNewlines).capitalizingFirstLetter()
+        if newName == "" {
+            newName = recipe.name
+        }
+        
+        var newIngredients = [String]()
+        for ingredient in recipe.ingredients {
+            if ingredient != "" {
+                newIngredients.append(ingredient.lowercased().trimmingCharacters(in: .whitespacesAndNewlines).capitalizingFirstLetter())
+            }
+        }
+        
+        var newInstructions = [String]()
+        for instruction in recipe.directions {
+            if instruction != "" {
+                newInstructions.append(instruction.lowercased().trimmingCharacters(in: .whitespacesAndNewlines).capitalizingFirstLetter())
+            }
+        }
         
         let db = Firestore.firestore()
         
-        let documentName = db.collection("sub recipes").document(docName)
-        documentName.setData(["name": name, "directions": directions, "ingredients": ingredients])
+        let documentName = db.collection("sub recipes").document(recipe.docName)
+        documentName.setData(["name": newName, "directions": newInstructions, "ingredients": newIngredients])
         
+        // Returns to the list view
         presentationMode.wrappedValue.dismiss()
     }
 }
 
 struct EditRecipeView_Previews: PreviewProvider {
     static var previews: some View {
-        let foods = RecipesData()
-        let recipe = foods.recipes[0]
-        
+        let data = RecipesData()
+        let recipe = data.recipes[0]
         EditRecipeView(recipe: recipe)
     }
 }
